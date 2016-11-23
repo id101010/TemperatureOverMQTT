bd2d393095bef80ee17f032d59e2ec0d71f6cf64
id101010 <aaron@duckpond.ch>
2016-11-23 14:29:01 +0100

    Added testproject

---
+++ MQTT_Gateway/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <signal.h>
+#include <pthread.h>
+
+static void *Thread1(void *pdata){
+    while(1){
+        printf("Ping\n");
+        fflush(stdout);
+        sleep(1);
+    }
+    pthread_exit(NULL);
+}
+
+static void *Thread2(void *pdata){
+    while(1){
+        printf("Pong\n");
+        fflush(stdout);
+        sleep(1);
+    }
+    pthread_exit(NULL);
+}
+
+int32_t main(void){
+
+    pthread_t Thread1ID, Thread2ID;
+
+    /* Create all Threads */
+    pthread_create(&Thread1ID, NULL, Thread1, NULL);
+    pthread_create(&Thread2ID, NULL, Thread2, NULL);
+
+    /* Wait 10s */
+    sleep(10);
+
+    pthread_cancel(Thread1ID);
+    pthread_join(Thread1ID, NULL);
+    pthread_cancel(Thread2ID);
+    pthread_join(Thread2ID, NULL);
+
+    exit(EXIT_SUCCESS);
+}
