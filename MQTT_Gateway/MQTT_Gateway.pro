11ade26de985ae79bf7715f6909a2f642b1f94d1
ingmacmech <marcus.schluep@kns.ch>
2016-12-04 13:21:55 +0100

    Upps now with code :)

--- MQTT_Gateway/MQTT_Gateway.pro
+++ MQTT_Gateway/MQTT_Gateway.pro
@@ -3,12 +3,17 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 LIBS += -pthread
+LIBS += -lpthread
+LIBS += -L$$PWD/lib/ -lpaho-mqtt3a
 
 target.path = /home/pi
 INSTALLS += target
 
 SOURCES += main.c \
-    ble_api.c
+    ble_api.c \
+    broker_api.c
 
 HEADERS += \
-    ble_api.h
+    ble_api.h \
+    broker_api.h \
+    libs/MQTTAsync.h
---
+++ MQTT_Gateway/broker_api.c
@@ -0,0 +1,73 @@
+#include "broker_api.h"
+
+void connlost(void *context, char *cause)
+{
+    MQTTAsync client = (MQTTAsync)context;
+    MQTTAsync_connectOptions conn_opts = MQTTAsync_connectOptions_initializer;
+
+    int rc;
+
+    printf("\nConnection lost\n");
+    printf("          cause: %s\n", cause);
+
+    printf("Reconnecting\n");
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+
+    if((rc = MQTTAsync_connect(client, &conn_opts)) != MQTTASYNC_SUCCESS)
+    {
+        printf("Failed to start connect, return code %d\n", rc);
+        finished = 1;
+    }
+}
+
+void onDisconnect(void* context, MQTTAsync_successData* response)
+{
+        printf("Successful disconnection\n");
+        finished = 1;
+}
+
+void onSend(void* context, MQTTAsync_successData* response)
+{
+        MQTTAsync client = (MQTTAsync)context;
+        MQTTAsync_disconnectOptions opts = MQTTAsync_disconnectOptions_initializer;
+        int rc;
+        printf("Message with token value %d delivery confirmed\n", response->token);
+        opts.onSuccess = onDisconnect;
+        opts.context = client;
+        if ((rc = MQTTAsync_disconnect(client, &opts)) != MQTTASYNC_SUCCESS)
+        {
+                printf("Failed to start sendMessage, return code %d\n", rc);
+                exit(-1);
+        }
+}
+
+void onConnectFailure(void* context, MQTTAsync_failureData* response)
+{
+        printf("Connect failed, rc %d\n", response ? response->code : 0);
+        finished = 1;
+}
+
+void onConnect(void* context, MQTTAsync_successData* response)
+{
+        MQTTAsync client = (MQTTAsync)context;
+        MQTTAsync_responseOptions opts = MQTTAsync_responseOptions_initializer;
+        MQTTAsync_message pubmsg = MQTTAsync_message_initializer;
+        int rc;
+        printf("Successful connection\n");
+
+        opts.onSuccess = onSend;
+        opts.context = client;
+        pubmsg.payload = PAYLOAD;
+        pubmsg.payloadlen = strlen(PAYLOAD);
+        pubmsg.qos = QOS;
+        pubmsg.retained = 0;
+        deliveredtoken = 0;
+        if ((rc = MQTTAsync_sendMessage(client, TOPIC, &pubmsg, &opts)) != MQTTASYNC_SUCCESS)
+        {
+                printf("Failed to start sendMessage, return code %d\n", rc);
+                exit(-1);
+        }
+}
+
+
