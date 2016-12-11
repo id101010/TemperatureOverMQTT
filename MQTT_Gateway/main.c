3e6e89ce9a86b64e956d9865ea6774dff94fd47a
ingmacmech <marcus.schluep@kns.ch>
2016-12-11 15:46:24 +0100

    Working Broker

--- MQTT_Gateway/main.c
+++ MQTT_Gateway/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include "ble_api.h"
 #include "json.h"
+#include "broker_api.h"
 
 int main(void)
 {
     // Define a connection
     connection_t conn;
+
+
+    // Broker
+    message_t message;
 
     // Init the connection object
     init_connect_obj(&conn);
@@ -14,7 +19,7 @@ int main(void)
     socket_get_connection(&conn);
 
     // Do a ble scan
-    //sensor_ble_scan(&conn);
+    sensor_ble_scan(&conn);
 
     // Connect to sensor
     sensor_connect(&conn, SENSOR_MAC);
@@ -28,6 +33,13 @@ int main(void)
     // Close socket
     close(conn.socket_fd);
 
+    // Broker
+    message.payload = "Hello World!!!!";
+    message.topic = TOPIC;
+
+    startBroker();
+    sendMQTTmessage(&message);
+    disconectBroker();
     // Exit
     return(EXIT_SUCCESS);
 }
