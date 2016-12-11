74f5ba4e3e557c63dd7c2524febd8e8b4c032677
id101010 <aaron@duckpond.ch>
2016-12-11 23:52:16 +0100

    Implemented buffered recieving

--- MQTT_Gateway/main.c
+++ MQTT_Gateway/main.c
@@ -14,10 +14,29 @@ connection_t conn;
 pthread_t listener;
 
 /*----- Function prototypes --------------------------------------------------*/
+void event_parser(json_t *jmsg);
+static void *threaded_listener(void *pdata);
 
 /*----- Data -----------------------------------------------------------------*/
 
 /*----- Implementation -------------------------------------------------------*/
+
+/*******************************************************************************
+ *  function :    event_parser
+ ******************************************************************************/
+/** @brief        Searches for the event-key and then parses its value.
+ *
+ *  @type         global
+ *
+ *  @param[in]    jmsg     json event object
+ *
+ *  @return       none
+ *
+ ******************************************************************************/
+void event_parser(json_t *jmsg)
+{
+
+}
 
 /*******************************************************************************
  *  function :    threaded_listener
@@ -35,18 +54,22 @@ pthread_t listener;
 static void *threaded_listener(void *pdata)
 {
     char tmp[STRING_SIZE];
-    int length = 0;
+    int i=0;
 
     while(true){
         // Try to read a response from the socket and store it in tmp
-        length = recv(conn.socket_fd, tmp, STRING_SIZE-1, 0);
-        tmp[length] = '\0';
-        // debug print
-        debug(RECV_MSG, tmp);
-        // sleep 1ms
+        int strlength = recv(conn.socket_fd, tmp, STRING_SIZE-1, 0);
+
+        int laststart = 0;
+        for(i = 0; i < strlength; i++) {
+            if(tmp[i]=='\n') { //end of one message
+                tmp[i] = '\0'; //terminate message here
+                debug(RECV_MSG, &tmp[laststart]);
+                laststart=i+1;
+            }
+        }
         usleep(1000L);
     }
-
     pthread_exit(NULL);
 }
 
